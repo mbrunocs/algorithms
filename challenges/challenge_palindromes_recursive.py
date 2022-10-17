9d23726c6344e955a1150533ac2ecfd3a3d8be8f
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-10-17 20:10:16 -0300

    dev: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if len(word) >= 100: raise(RecursionError)
+    mirror = ''
+    index = high_index
+    while index >= low_index:
+        mirror += word[index]
+        index -= 1
+    return False if mirror != word or word == '' else True

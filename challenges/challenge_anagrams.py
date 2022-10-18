1537a83f8ce96649f36be778bb1eab005b5cd6de
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-10-17 22:51:15 -0300

    dev: minimize is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,15 +1,19 @@
+def dict_letters(word):
+    letter = dict()
+    for char in word:
+        if char in letter:
+            letter[char] += 1
+        else:
+            letter[char] = 1
+    return letter
+
 def is_anagram(first_string, second_string):
-    word = dict()
     if len(second_string) == 0 or len(first_string) == 0:
         return False
     try:
-        for c in first_string.casefold():
-            if c in word:
-                word[c] += 1
-            else:
-                word[c] = 1
+        word = dict_letters(first_string.casefold())
         for c in second_string.casefold():
-            if not c in word or word[c] == 0:
+            if c not in word or word[c] == 0:
                 return False
             word[c] -= 1
         return True

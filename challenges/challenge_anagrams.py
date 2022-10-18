f94c0a7c17ac6d9f6aeae00f1252e9f67469ca56
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-10-17 22:29:26 -0300

    is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    word = dict()
+    if len(second_string) == 0 or len(first_string) == 0:
+        return False
+    try:
+        for c in first_string.casefold():
+            if c in word:
+                word[c] += 1
+            else:
+                word[c] = 1
+        for c in second_string.casefold():
+            if not c in word or word[c] == 0:
+                return False
+            word[c] -= 1
+        return True
+    except KeyError:
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if len(word) >= 100: raise(RecursionError)
+    if len(word) >= 100:
+        raise (RecursionError)
     mirror = ''
     index = high_index
     while index >= low_index:
